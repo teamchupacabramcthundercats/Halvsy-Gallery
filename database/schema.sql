5c45c3ff818cfe89b9fa56f7795bc1eeeac6baa8
Joel Carpenter <aldosac@gmail.com>
2020-08-06 16:19:31 -0700

    Creates schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE gallery;
+
+USE gallery;
+
+CREATE TABLE `products` (
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `is_favorite` BOOLEAN,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `images` (
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `product_id` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
+)
+
+ALTER TABLE `images`
+  ADD CONSTRAINT FOREIGN KEY (`product_id`)
+  REFERENCES `products` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
