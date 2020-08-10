d0f0d38bddc27811e37a5481e93503a119ba68a0
Joel Carpenter <aldosac@gmail.com>
2020-08-10 11:37:14 -0700

    Resolves bugs with seeding db
    There were a couple of spots that were still using the wrong
    variable reference from before the files were linted.
    Corrected all of those, now passing all tests.

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 module.exports.db = mysql.createConnection({
-  host: `127.0.0.1`,
-  user: `student`,
-  password: `student`,
-  database: `gallery`
-})
+  host: '127.0.0.1',
+  user: 'student',
+  password: 'student',
+  database: 'gallery',
+});
 
 module.exports.db.queryAsync = (QUERY) => new Promise((resolve, reject) => {
   module.exports.db.query(QUERY, (err, result) => {
