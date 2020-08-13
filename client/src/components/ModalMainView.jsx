a5d502652a532960aef823f070f4a8a226e88d63
Joel Carpenter <aldosac@gmail.com>
2020-08-13 11:05:49 -0700

    Adds images to ModalMainView

--- client/src/components/ModalMainView.jsx
+++ client/src/components/ModalMainView.jsx
@@ -6,13 +6,34 @@ const ModalMainView = (props) => {
 
   return (
     <div className="modal-main-view">
-      MainViewImage
+      {images.map((image) => {
+        if (image === currentImage) {
+          return (
+            <MainViewImage
+              key={counter++}
+              counter={counter}
+              image={image}
+              display="inline-block"
+              onClickHandler={onClickToShowModal}
+            />
+          );
+        } else {
+          return (
+            <MainViewImage
+              key={counter++}
+              counter={counter}
+              image={image}
+              display="none"
+            />
+          );
+        }
+      })}
     </div>
-  )
+  );
 };
 
 ModalMainView.propTypes = {
-
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default ModalMainView;
